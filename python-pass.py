9658b529c9b998ef086470dbdae778143189f4e6
sojey20 <70294194+sojey20@users.noreply.github.com>
2021-10-01 00:57:19 +0300

    Update python-pass.py

--- python-pass.py
+++ python-pass.py
@@ -4,4 +4,24 @@ class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        def longest_palindromic(s: str) -> str:
+        res = ""
+
+        if len(s) == 1:
+            print(s)
+        else:
+            for v in range(1, len(s)):
+                if s == s[::-1]:
+                    if len(s) >= len(res):
+                        res = s
+                    break
+                else:
+                    for i in range(1, len(s)):
+                        subString = s[i:]
+                        if subString == subString[::-1]:
+                            if len(subString) >= len(res):
+                                res = subString
+                            break
+                s = s[:-v]
+        print(res)
+Solution.longest_palindromic(s="babad")
